bccfb97d2f4e0c14cf49412a42614a8c6f0d03d7
kabazoka <73285479+kabazoka@users.noreply.github.com>
2024-10-10 00:57:34 +0800

    update download.sh

--- download.sh
+++ download.sh
@@ -15,15 +15,15 @@ fi
 
 # Replace 'YOUR_ZIP_FILE_ID' with the actual file ID from Google Drive
 # https://drive.google.com/file/d/15IcrG7DWwPjqEKBCbzt3H6TLtXu9J8bL/view?usp=sharing
-ZIP_FILE_ID=15IcrG7DWwPjqEKBCbzt3H6TLtXu9J8bL
+ZIP_FILE_ID=11c2FD6ik081G25LxvyznuBFTB1DDKsFr
 
 # Download the zip file
 echo "Downloading the zip file containing inference.py and models..."
 python3 -m gdown --id $ZIP_FILE_ID -O models_and_code.zip
 
 # Unzip the file
-echo "Extracting the zip file..."
-unzip -o models_and_code.zip
-rm models_and_code.zip
+#echo "Extracting the zip file..."
+#unzip -o models_and_code.zip
+#rm models_and_code.zip
 
-echo "Download and extraction complete."
+#echo "Download and extraction complete."
